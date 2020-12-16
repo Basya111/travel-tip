176b9417639f39186e83a6081b74db01234d0a28
Basya111 <mail4basy@gmail.com>
2020-12-16 18:57:52 +0200

    add functions

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,5 +1,5 @@
 
-import {locationService} from './services/location-service.js'
+import { locationService } from './services/location-service.js'
 
 
 console.log('locationService', locationService);
@@ -13,19 +13,35 @@ window.onload = () => {
         })
         .catch(console.log('INIT MAP ERROR'));
 
-    getUserPosition()
-        .then(pos => {
-            console.log('User position is:', pos.coords);
-        })
-        .catch(err => {
-            console.log('err!!!', err);
-        })
 
-    document.querySelector('.btn').addEventListener('click', (ev) => {
-        console.log('Aha!', ev.target);
-        panTo(35.6895, 139.6917);
+
+    const elBtnHome = document.querySelector('.home-btn');
+
+    elBtnHome.addEventListener('click', onGoHome);
+
+
+
+
+    document.querySelector('.nav-to-btn').addEventListener('click', (ev) => {
+        var destLocation = document.querySelector('input[name=destination]').value
+        // todo API google geo code get lat, lng
+        var googleGoToUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${destLocation}&key=AIzaSyAW0VHtJ_ObPHzi_jtsYKaB9vJgarQC4po`
+        var prmGoTo = locationService.getData(googleGoToUrl)
+            .then(data => data.results[0].geometry.location)
+            .then(pos => {
+                panTo(pos.lat, pos.lng);
+                addMarker(pos)
+
+            })
+        console.log('Aha!', prmGoTo);
+        document.querySelector('input[name=destination]').value = ''
     })
+
+    locationService.createPlaces()
+    renderPlaces()
 }
+
+
 
 
 export function initMap(lat = 32.0749831, lng = 34.9120554) {
@@ -39,8 +55,18 @@ export function initMap(lat = 32.0749831, lng = 34.9120554) {
                 zoom: 15
             })
             console.log('Map!', gGoogleMap);
+
+            gGoogleMap.addListener('click', (ev) => {
+                console.log('Map clicked', ev);
+                const placeName = prompt('Place name?')
+                if (!placeName) return;
+                locationService.savePlace(placeName, ev.latLng.lat(), ev.latLng.lng())
+                console.log('Map clicked', placeName, ev.latLng.lat(), ev.latLng.lng());
+                renderPlaces(placeName, ev.latLng.lat(), ev.latLng.lng())
+            })
         })
 }
+
 
 function addMarker(loc) {
     var marker = new google.maps.Marker({
@@ -48,12 +74,26 @@ function addMarker(loc) {
         map: gGoogleMap,
         title: 'Hello World!'
     });
+    console.log(loc);
     return marker;
 }
 
 function panTo(lat, lng) {
     var laLatLng = new google.maps.LatLng(lat, lng);
     gGoogleMap.panTo(laLatLng);
+}
+
+function onGoHome() {
+    getUserPosition()
+        .then(pos => {
+            console.log('User position is:', pos.coords);
+            var position = { lat: pos.coords.latitude, lng: pos.coords.longitude }
+            panTo(pos.coords.latitude, pos.coords.longitude)
+            addMarker(position)
+        })
+        .catch(err => {
+            console.log('err!!!', err);
+        })
 }
 
 function getUserPosition() {
@@ -64,9 +104,10 @@ function getUserPosition() {
 }
 
 
+
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDLOgg8zvYAvrFZFqiCJErIweRG7c7suhM';
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
@@ -77,6 +118,46 @@ function _connectGoogleApi() {
         elGoogleApi.onerror = () => reject('Google script failed to load')
     })
 }
+
+function renderPlaces() {
+    var places = locationService.getPlacesToDisplay();
+    var elListPlace = document.querySelector('.list-places');
+    var strHtmls = places.map(place => `<div class="place">
+    <div class="place-img"><img src="img/navigation.png" /></div>
+    <div class = "place-name">${place.placeName}</div>
+    <div><button class="del-btn">x</button></div></div>`)
+    // var strHtmls = places.map(place => `<div class="place"><button onclick="onNavigate(${place.lat},${place.lng})"><img src = "img/navigation.jpg"></button>
+    // ${place.placeName}<button onclick="onDeletePlace(${place.id})">x</button></div>`)
+    console.log(strHtmls, elListPlace);
+    var strHtml = strHtmls.join('')
+    elListPlace.innerHTML = strHtml;
+
+    var elPlaces = document.querySelectorAll('.place')
+    elPlaces.forEach((elPlace, idx) => {
+        elPlace.addEventListener('click', () => {
+            onNavigate(places[idx].lat, places[idx].lng)
+        })
+    })
+    var elDelPlaces = document.querySelectorAll('.del-btn')
+    elDelPlaces.forEach((elDelPlace, idx) => {
+        elDelPlace.addEventListener('click', () => {
+            onDeletePlace(places[idx].id)
+        })
+    })
+}
+
+function onDeletePlace(placeId) {
+    locationService.deletePlace(placeId);
+    renderPlaces();
+}
+
+
+function onNavigate(lat, lng) {
+    console.log('lat', lat, 'lng', lng);
+    initMap(lat, lng);
+}
+
+
 
 
 
--- js/services/location-service.js
+++ js/services/location-service.js
@@ -1,11 +1,103 @@
 
 export const locationService = {
-    getLocations
+    getData,
+    getLocations,
+    showLocation,
+    handleLocationError,
+    createPlaces,
+    getPlacesToDisplay,
+    savePlace,
+    deletePlace,
+    saveToStorage,
+    loadFromStorage
+
 }
 
 
+const KEY_PLACES = 'placesDB'
+var gPlaces;
+var gNextId;
+
 const  gLocations = [{lat: 17, lng: 19, name: 'Puki Home'}];
+
+export function getData(url) {
+    return axios.get(url)
+        .then(res => res.data)
+}
 
 function getLocations() {
     return Promise.resolve(gLocations)
+}
+
+function showLocation(position) {
+    initMap(position.coords.latitude, position.coords.longitude, 15);
+}
+
+function handleLocationError(error) {
+    var locationError = document.getElementById("locationError");
+
+    switch (error.code) {
+        case 0:
+            locationError.innerHTML = "There was an error while retrieving your location: " + error.message;
+            break;
+        case 1:
+            locationError.innerHTML = "The user didn't allow this page to retrieve a location.";
+            break;
+        case 2:
+            locationError.innerHTML = "The browser was unable to determine your location: " + error.message;
+            break;
+        case 3:
+            locationError.innerHTML = "The browser timed out before retrieving the location.";
+            break;
+    }
+}
+
+function createPlaces () {
+    var places = loadFromStorage(KEY_PLACES);
+    if (!places || !places.length) {
+        gNextId = 101;
+        places = [];
+    }
+    else {
+        gNextId = places[places.length-1].id+1;
+    }
+    gPlaces = places;
+    saveToStorage(KEY_PLACES,gPlaces);
+}
+
+function getPlacesToDisplay() {
+    return gPlaces;
+}
+
+function savePlace (placeName, lat, lng) {
+    var place = {placeName, lat, lng}
+    place.id = ++gNextId;
+    place.weather = '';
+    place.createdAt = getTime()
+    place.updatedAt = updateTime()
+    gPlaces.push(place)
+    saveToStorage (KEY_PLACES, gPlaces);
+}
+
+function deletePlace (placeId) {
+    var idx  = gPlaces.findIndex(place => place.id===+placeId)
+    gPlaces.splice(idx,1)
+    saveToStorage(KEY_PLACES,gPlaces)
+}
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
+
+function getTime(){
+    return Date.now
+}
+
+function updateTime(){
+    return Date.now
 }
